43185dc7d9f43f5937d17294c364cb555c6c7f7a
minhazur9 <minhazur9@gmail.com>
2021-04-02 02:07:17 -0400

    added skills section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,55 @@ export default () => <div className={styles}>
     <Navbar />
     <div className="intro-container">
         <div className="intro">
-            <h2>Full Stack Developer</h2>
+            <h1>Full Stack Developer</h1>
             <p>Making scalable and reliable applications </p>
         </div>
+    </div>
+    <div className="skills">
+        <div className="skill-cols">
+            <div className="skill-cat" >
+                <h4>Languages</h4>
+                <div className="list-table">
+                    <p>JavaScript</p>
+                    <p>Python</p>
+                    <p>TypeScript</p>
+                    <p>HTML</p>
+                    <p>SQL</p>
+                    <p>NoSQL</p>
+                    <p>CSS</p>
+                    <p>SCSS</p>
+                    <p>Java</p>
+                    <p>C++</p>
+                    <p>PHP</p>
+                </div>
+            </div>
+            <div className="skill-cat" >
+                <h4>Libaries & Frameworks</h4>
+                <div className="list-table">
+                    <p>React.js</p>
+                    <p>Redux</p>
+                    <p>Node.js</p>
+                    <p>Express.js</p>
+                    <p>MongoDB</p>
+                    <p>PostgreSQL</p>
+                    <p>GraphQL</p>
+                    <p>Apollo</p>
+                    <p>Bootstrap.css</p>
+                    <p>Materialize.css</p>
+                    <p>ReactGatsby</p>
+                </div>
+            </div>
+            <div className="skill-cat" >
+                <h4>Tools</h4>
+                <div className="list-table">
+                    <p>Git</p>
+                    <p>Apache</p>
+                    <p>Heroku</p>
+                    <p>Jira</p>
+                </div>
 
+            </div>
+        </div>
     </div>
 
 </div>;
