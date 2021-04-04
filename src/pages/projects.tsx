a271a35e0f028ed108a36c32bd5a8dc2fe28865b
minhazur9 <minhazur9@gmail.com>
2021-04-04 03:32:40 -0400

    renders project titles now

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,8 +2,22 @@ import React from 'react'
 import Layout from '../components/Layout'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { graphql } from 'gatsby'
 
-const Projects = () => {
+const Projects = ({ data }: any) => {
+    const allProjects: any = data.allMarkdownRemark.nodes;
+    const renderProjects: any = () => {
+        return allProjects.map((project: any) => {
+            const { title } = project.frontmatter
+            return (
+                <div key={title} >
+                    <h3>{title}</h3>
+                </div>
+
+            )
+        })
+    }
+
     return (
         <Layout>
             <div className="projects-container-header">
@@ -12,10 +26,25 @@ const Projects = () => {
                     <FontAwesomeIcon icon={faSearch} size='1x' className="search-icon" />
                     <input type="text" name="search-projects" id="search-projects" />
                 </div>
-
+            </div>
+            <div className="projects-list">
+                {renderProjects()}
             </div>
         </Layout>
     )
 }
+
+export const query = graphql`
+query MyQuery {
+    allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
+      nodes {
+        frontmatter {
+          title
+        }
+      }
+    }
+  }
+  
+`
 
 export default Projects
---
+++ src/projects/premier.md
@@ -0,0 +1,14 @@
+---
+title: Premier
+stack: React, Django, PostgreSQL
+date: 2020-12-03T00:00:00+00:00
+thumb: ../images/project-images/premier-thumb.png
+live: https://premier-min.herokuapp.com/
+github: https://github.com/minhazur9/premier
+---
+
+A movie/show database application that allows you to look up information about movies and tv shows. You can read reviews and make your own reviews by simply registering for an account and logging in. You also have the ability to add in a list of recommendations for movies and tv shows on your profile.
+
+Tech Stack: React, Django, PostgreSQL
+
+Live: https://premier-min.herokuapp.com/
